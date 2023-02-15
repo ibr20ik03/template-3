3354071aa865a0a18ce35a34f1fbfe666655b619
ibr20ik03 <125470491+ibr20ik03@users.noreply.github.com>
2023-02-15 20:23:23 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+//mega_menu
+// let mega_menu = document.querySelector(".toggle-menu");
+
+// mega_menu.addEventListener("click", () => {
+//     let ul_list = document.querySelector(".toggle-menu ul");
+//     if (ul_list.className == "hide") {
+//         ul_list.style.display= "flex";
+//         ul_list.classList.add("x");
+//     }else {
+//         ul_list.style.display= "none";
+//         ul_list.classList.remove("x");
+//     }
+// })
+
